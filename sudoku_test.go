5ec8913d3eca3a5034f01476c09a8718d35cf9f5
Shantanu <joshishantanu4@gmail.com>
2016-01-19 05:36:16 -0800

    modify tests

--- sudoku_test.go
+++ sudoku_test.go
@@ -26,12 +26,42 @@ func TestFixRow(t *testing.T) {
 	s := New(n)
 	s.board[2] = []int{3, 3, 2, 2}
 	row := s.board[2]
-	s.fixRow(2)
+	s.fixRow(2, false)
 	fixedRow1 := []int{3, 1, 2, 4}
 	fixedRow2 := []int{3, 4, 2, 1}
 	if !(reflect.DeepEqual(row, fixedRow1) || reflect.DeepEqual(row, fixedRow2)) {
 		t.Errorf("did not replace all twos: %v", row)
 	}
+	if !s.modified {
+		t.Errorf("expected %t but got %t", true, false)
+	}
+}
+
+func TestFixCol(t *testing.T) {
+	n := 4
+	s := New(n)
+	s.board[0] = []int{1, 2, 1, 4}
+	s.board[1] = []int{2, 3, 1, 4}
+	s.board[2] = []int{3, 2, 3, 3}
+	s.board[3] = []int{4, 3, 3, 1}
+
+	colIndex := 2
+	s.fixCol(colIndex, false)
+
+	fixedCol1 := []int{2, 1, 4, 3}
+	fixedCol2 := []int{4, 1, 2, 3}
+
+	col := make([]int, s.n)
+	for i := 0; i < s.n; i++ {
+		col[i] = s.board[i][colIndex]
+	}
+
+	if !(reflect.DeepEqual(col, fixedCol1) || reflect.DeepEqual(col, fixedCol2)) {
+		t.Errorf("did not replace all twos: %v", col)
+	}
+
+	t.Logf("%v", col)
+
 	if !s.modified {
 		t.Errorf("expected %t but got %t", true, false)
 	}
